18d7b72e7828bd5a10818d8efd29b5f6e0e21383
nosu <i@nosux.xyz>
2016-11-25 13:23:40 +0900

    Dialogのサンプルを実装

--- bot-fw-csharp/Controllers/MessagesController.cs
+++ bot-fw-csharp/Controllers/MessagesController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
 using Newtonsoft.Json;
 
 namespace bot_fw_csharp
@@ -19,22 +20,78 @@ namespace bot_fw_csharp
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await Conversation.SendAsync(activity, () => new EchoDialog());
             }
             else
             {
                 HandleSystemMessage(activity);
             }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
+            //if (activity.Type == ActivityTypes.Message)
+            //{
+            //    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            //    // calculate something for us to return
+            //    int length = (activity.Text ?? string.Empty).Length;
+
+            //    // return our reply to the user
+            //    Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+            //    await connector.Conversations.ReplyToActivityAsync(reply);
+            //}
+            //else
+            //{
+            //    HandleSystemMessage(activity);
+            //}
+            //var response = Request.CreateResponse(HttpStatusCode.OK);
+            //return response;
+        }
+
+        [Serializable]
+        public class EchoDialog : IDialog<object>
+        {
+            protected int resetCount = 1;
+            protected int otherCount = 1;
+
+            public async Task StartAsync(IDialogContext context)
+            {
+                context.Wait(MessageReceivedAsync);
+            }
+
+            public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+            {
+                var message = await argument;
+                if (message.Text == "リセット")
+                {
+                    PromptDialog.Confirm(
+                        context,
+                        AfterResetAsync,
+                        "本当にリセットしてよろしいですか?",
+                        "「はい」または「いいえ」でお答えください",
+                        promptStyle: PromptStyle.None);
+                }
+                else
+                {
+                    await context.PostAsync($"'{message.Text}'と{this.otherCount++}回言いました");
+                    context.Wait(MessageReceivedAsync);
+                }
+            }
+
+            public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+            {
+                var confirm = await argument;
+                if (confirm)
+                {
+                    this.resetCount = 1;
+                    await context.PostAsync($"{this.resetCount}回目のリセットを実行しました");
+                }
+                else
+                {
+                    await context.PostAsync("リセットを中止しました");
+                }
+                context.Wait(MessageReceivedAsync);
+            }
         }
 
         private Activity HandleSystemMessage(Activity message)
